628c1b7230f27d237a79ecd0419d429a087b4221
Ciro Lima <caiazzo14@gmail.com>
2020-02-20 11:41:35 -0300

    Used by andre to show me details of the router functions

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    restaurant = Restaurant.new(params)
+    restaurant.save
+    redirect_to restaurant.path(restaurant)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    member do
+      resources :review
+    end
+  end
 end
